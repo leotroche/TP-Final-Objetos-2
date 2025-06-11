799105011c8c1a3b343371fb675542c6ef5fa800
Leo Troche <leo.m.troche@gmail.com>
2025-06-11 19:33:13 -0300

    Agregado proyecto base

---
+++ TP_FINAL_POO_2/test/busqueda/FiltroTest.java
@@ -0,0 +1,19 @@
+package busqueda;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FiltroTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TP_FINAL_POO_2/test/muestra/MuestraTest.java
@@ -0,0 +1,19 @@
+package muestra;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MuestraTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

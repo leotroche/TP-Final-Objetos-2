794dd518bc2de1dba4cc0318e658c195ade1bf1b
Leo Troche <leo.m.troche@gmail.com>
2025-06-12 17:18:56 -0300

    Agregado metodos sin implementar en Muestra

--- TP_FINAL_POO_2/src/muestra/Muestra.java
+++ TP_FINAL_POO_2/src/muestra/Muestra.java
@@ -1,5 +1,27 @@
 package muestra;
 
-public class Muestra {
+import java.time.LocalDate;
 
+import varios.TipoDeInsecto;
+
+public class Muestra {
+	public LocalDate getFechaDeCreacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public EstadoDeVerificacion getEstadoDeVerificacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public LocalDate getFechaDeUltimaVotacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public TipoDeInsecto getTipoDeInsectoDetectado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

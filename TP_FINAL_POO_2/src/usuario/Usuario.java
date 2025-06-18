75182b3fef7ab10400f02618c5335c308a9d34a9
Daniel Cáceres <dragonaladodera2002@hotmail.com>
2025-06-18 17:51:23 -0300

    Clase Usuario basica con getter y setter, y sus respectivos test

--- TP_FINAL_POO_2/src/usuario/Usuario.java
+++ TP_FINAL_POO_2/src/usuario/Usuario.java
@@ -1,5 +1,27 @@
 package usuario;
 
-public class Usuario {
 
+public class Usuario {
+	private String nombre;
+	private EstadoDeConocimiento estadoDeConocimiento;
+	
+	public Usuario(String nombre) {
+		this.setNombre(nombre);
+	}
+	
+	public void setNombre(String n) {
+		this.nombre = n;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public EstadoDeConocimiento getEstadoDeConocimiento() {
+		return this.estadoDeConocimiento;
+	}
+	
+	public void setEstadoDeConocimiento(EstadoDeConocimiento estado) {
+		this.estadoDeConocimiento = estado;
+	}
 }

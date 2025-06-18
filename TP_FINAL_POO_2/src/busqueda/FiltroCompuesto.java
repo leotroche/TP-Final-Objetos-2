cbfb35922f59f7a7975eaa6d14137772f9287f6d
Leo Troche <leo.m.troche@gmail.com>
2025-06-18 11:55:18 -0300

    Refactor: FiltroCompuesto hereda de Filtro

--- TP_FINAL_POO_2/src/busqueda/FiltroCompuesto.java
+++ TP_FINAL_POO_2/src/busqueda/FiltroCompuesto.java
@@ -3,7 +3,7 @@ package busqueda;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class FiltroCompuesto implements Filtro {
+public abstract class FiltroCompuesto extends Filtro {
 	private List<Filtro> filtros = new ArrayList<Filtro>();
 
 	public FiltroCompuesto(Filtro... filtros) {

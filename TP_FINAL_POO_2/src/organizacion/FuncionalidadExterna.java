f06ffc0736ed1b29d4c7542cb80387d134803ac2
Leo Troche <leo.m.troche@gmail.com>
2025-06-20 14:31:52 -0300

    Agregado interface FuncionalidadExterna y enum TipoDeOrganizacion

---
+++ TP_FINAL_POO_2/src/organizacion/FuncionalidadExterna.java
@@ -0,0 +1,7 @@
+package organizacion;
+
+import muestra.Muestra;
+
+public interface FuncionalidadExterna {
+	public void nuevoEvento(Organizacion organizacion, ZonaDeCobertura zonaDeCobertura, Muestra muestra);
+}

4f690564525937790507314ed09dafc3872b6262
Leo Troche <leo.m.troche@gmail.com>
2025-06-23 01:01:43 -0300

    Agregado GestorDeFuncionalidades y RegistroDeFuncionalidad con métodos para gestionar y ejecutar funcionalidades externas.

---
+++ TP_FINAL_POO_2/src/gestores/funcionalidades/RegistroDeFuncionalidad.java
@@ -0,0 +1,42 @@
+package gestores.funcionalidades;
+
+import eventos.Evento;
+import muestra.Muestra;
+import organizacion.Organizacion;
+import organizacion.ZonaDeCobertura;
+
+public class RegistroDeFuncionalidad {
+	private Evento evento;
+	private FuncionalidadExterna funcionalidad;
+
+	public RegistroDeFuncionalidad(Evento evento, FuncionalidadExterna funcionalidad) {
+		this.setEvento(evento);
+		this.setFuncionalidad(funcionalidad);
+	}
+
+	private Evento getEvento() {
+		return this.evento;
+	}
+
+	private FuncionalidadExterna getFuncionalidad() {
+		return this.funcionalidad;
+	}
+
+	private void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+	private void setFuncionalidad(FuncionalidadExterna funcionalidad) {
+		this.funcionalidad = funcionalidad;
+	}
+
+	public boolean correspondeA(Evento evento) {
+		return this.getEvento().equals(evento);
+	}
+
+	public void ejecutar(Organizacion organizacion, ZonaDeCobertura zonaDeCobertura, Muestra muestra) {
+		if (this.getFuncionalidad() != null) {
+			this.getFuncionalidad().nuevoEvento(organizacion, zonaDeCobertura, muestra);
+		}
+	}
+}

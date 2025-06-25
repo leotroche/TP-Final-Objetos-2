cdc1751e5ef81bea7e3f389d6d2e9ba540391c5e
Pablo Misiukowiec <102833227+pablomm94@users.noreply.github.com>
2025-06-25 17:52:36 -0300

    Update EstadoDeVerificacion.java
    Agregada interfaz EstadoDeVerificacion

--- TP_FINAL_POO_2/src/muestra/EstadoDeVerificacion.java
+++ TP_FINAL_POO_2/src/muestra/EstadoDeVerificacion.java
@@ -1,5 +1,16 @@
 package muestra;
+ 
+import varios.Opinion;
+import varios.TipoDeInsecto;
 
 public interface EstadoDeVerificacion {
 
+	public void agregarOpinion(Muestra muestra, Opinion opinion);
+	
+	void recibioOpinionDeExperto(Muestra muestra);
+	
+	public TipoDeInsecto obtenerResultadoActual(Muestra muestra);
 }
+
+
+ 

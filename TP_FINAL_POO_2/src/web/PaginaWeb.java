580616db836caf5732eaf8ec1421844e452289f1
Daniel Cáceres <dragonaladodera2002@hotmail.com>
2025-06-27 19:21:26 -0300

    Eliminacion de GestorDeEventos en la clase PaginaWeb

--- TP_FINAL_POO_2/src/web/PaginaWeb.java
+++ TP_FINAL_POO_2/src/web/PaginaWeb.java
@@ -4,29 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import eventos.Evento;
-import gestores.eventos.GestorDeEventos;
 import muestras.Muestra;
 import usuarios.Usuario;
 import zonas.ZonaDeCobertura;
 
 public class PaginaWeb {
-	private GestorDeEventos gestorDeEventos;
 	private ArrayList<Usuario> usuariosRegistrados= new ArrayList<>();
 	private ArrayList<Muestra> muestrasRegistradas = new ArrayList<>();
 
-	public PaginaWeb(GestorDeEventos gestorDeEventos) {
-		this.setGestorDeEventos(gestorDeEventos);
-	}
-
 	// ------------------------------------------------------------
-
-	private GestorDeEventos getGestorDeEventos() {
-		return this.gestorDeEventos;
-	}
-
-	private void setGestorDeEventos(GestorDeEventos gestorDeEventos) {
-		this.gestorDeEventos = gestorDeEventos;
-	}
 
 	public List<Usuario> getUsuariosRegistrados() {
 		return this.usuariosRegistrados;
@@ -42,7 +28,6 @@ public class PaginaWeb {
 
 	public void agregarMuestra(Muestra muestra) {
 		this.getMuestrasRegistradas().add(muestra);
-		this.getGestorDeEventos().notificar(Evento.MUESTRA_CARGADA, null, muestra);
 	}
 
 	// ------------------------------------------------------------
@@ -50,14 +35,14 @@ public class PaginaWeb {
 	// ------------------------------------------------------------
 
 	public void subscribirZonaDeCobertura(Evento evento, ZonaDeCobertura zona) {
-		this.getGestorDeEventos().suscribir(evento, zona);
+		//
 	}
 
 	public void desubscribirZonaDeCobertura(Evento evento, ZonaDeCobertura zona) {
-		this.getGestorDeEventos().desuscribir(evento, zona);
+		//
 	}
 	public void notificarZonaDeCobertura(Evento evento, ZonaDeCobertura zona, Muestra muestra) {
-		this.getGestorDeEventos().notificar(evento, zona, muestra);
+		//
 	}
 }
 

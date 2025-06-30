ecaa645a0ec7b1e463d14b553d399325b19103bd
Leo Troche <leo.m.troche@gmail.com>
2025-06-30 08:13:49 -0300

    Eliminado GestorDeFuncionalidades y métodos relacionados en clase Organizacion

--- TP_FINAL_POO_2/src/organizaciones/Organizacion.java
+++ TP_FINAL_POO_2/src/organizaciones/Organizacion.java
@@ -1,25 +1,15 @@
 package organizaciones;
 
-import eventos.Evento;
 import gestores.eventos.Observer;
-import gestores.funcionalidades.FuncionalidadExterna;
-import gestores.funcionalidades.GestorDeFuncionalidades;
-import muestras.Muestra;
 import ubicaciones.Ubicacion;
-import zonas.ZonaDeCobertura;
 
 public class Organizacion implements Observer {
 	private int cantEmpleados;
-	private GestorDeFuncionalidades gestorDeFuncionalidades;
 	private Ubicacion ubicacion;
 	private TipoDeOrganizacion tipoDeOrganizacion;
 
-	public Organizacion(
-			TipoDeOrganizacion tipoDeOrganizacion, Ubicacion ubicacion,
-			int cantDeEmpleados, GestorDeFuncionalidades gestorDeFuncionalidades
-			) {
+	public Organizacion(TipoDeOrganizacion tipoDeOrganizacion, Ubicacion ubicacion, int cantDeEmpleados) {
 		this.setCantEmpleados(cantDeEmpleados);
-		this.setGestorDeFuncionalidades(gestorDeFuncionalidades);
 		this.setUbicacion(ubicacion);
 		this.setTipoDeOrganizacion(tipoDeOrganizacion);
 	}
@@ -38,10 +28,6 @@ public class Organizacion implements Observer {
 		return this.cantEmpleados;
 	}
 
-	private GestorDeFuncionalidades getGestorDeFuncionalidades() {
-		return this.gestorDeFuncionalidades;
-	}
-
 	private void setTipoDeOrganizacion(TipoDeOrganizacion tipoDeOrganizacion) {
 		this.tipoDeOrganizacion = tipoDeOrganizacion;
 	}
@@ -52,26 +38,5 @@ public class Organizacion implements Observer {
 
 	private void setCantEmpleados(int cantEmpleados) {
 		this.cantEmpleados = cantEmpleados;
-	}
-
-	private void setGestorDeFuncionalidades(GestorDeFuncionalidades gestorDeFuncionalidades) {
-		this.gestorDeFuncionalidades = gestorDeFuncionalidades;
-	}
-
-	// ------------------------------------------------------------
-	// Metodos de eventos
-	// ------------------------------------------------------------
-
-	@Override
-	public void update(Evento evento, ZonaDeCobertura zonaDeCobertura, Muestra muestra) {
-		this.getGestorDeFuncionalidades().ejecutarPara(evento, this, zonaDeCobertura, muestra);
-	}
-
-	// ------------------------------------------------------------
-	// Metodos de estrategia de funcionalidades
-	// ------------------------------------------------------------
-
-	public void cambiarFuncionalidadPara(Evento evento, FuncionalidadExterna funcionalidad) {
-		this.getGestorDeFuncionalidades().cambiarPara(evento, funcionalidad);
 	}
 }

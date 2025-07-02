54974486eb1daa94e1a8ce66ae779ec57236d916
Daniel Cáceres <dragonaladodera2002@hotmail.com>
2025-07-02 15:50:59 -0300

    Modificacion final del test de PaginaWeb

--- TP_FINAL_POO_2/test/web/PaginaWebTest.java
+++ TP_FINAL_POO_2/test/web/PaginaWebTest.java
@@ -1,7 +1,9 @@
 package web;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,72 +15,95 @@ import zonas.ZonaDeCobertura;
 class PaginaWebTest {
 	private PaginaWeb web;
 
-	private Usuario usuario;
-	private Muestra muestra;
-
-
 	@BeforeEach
 	void setUp() {
-		muestra = mock(Muestra.class);
-		usuario = mock(Usuario.class);
-
 		web = new PaginaWeb();
 	}
 
+	// ------------------------------------------------------------
+
 	@Test
-	void unaPaginaWebIncialmenteNoTieneUsuarios() {
+	void unaPaginaWebIncialmenteNoTieneUsuariosRegistrados() {
 		assertTrue(web.getUsuariosRegistrados().isEmpty());
 	}
 
 	@Test
-	void unaPaginaWebIncialmenteNoTieneMuestras() {
-		assertTrue(web.getMuestrasRegistradas().isEmpty());
-	}
-
-	@Test
 	void agregarUsuarioAgregaUnUsuarioAlaPaginaWeb() {
+		Usuario usuario = mock(Usuario.class);
+
 		web.agregarUsuario(usuario);
 		assertEquals(1, web.getUsuariosRegistrados().size());
 	}
 
 	@Test
+	void agregarUsuarioNoAgregaElMismoUsuarioDosVeces() {
+		Usuario usuario = mock(Usuario.class);
+
+		web.agregarUsuario(usuario);
+		web.agregarUsuario(usuario);
+		assertEquals(1, web.getUsuariosRegistrados().size());
+	}
+
+	// ------------------------------------------------------------
+
+	@Test
+	void unaPaginaWebIncialmenteNoTieneMuestrasRegistradas() {
+		assertTrue(web.getMuestrasRegistradas().isEmpty());
+	}
+
+	@Test
 	void agregarMuestraAgregaUnaMuestraAlaPaginaWeb() {
+		Muestra muestra = mock(Muestra.class);
+
 		web.agregarMuestra(muestra);
 		assertEquals(1, web.getMuestrasRegistradas().size());
 	}
 
 	@Test
-	void noSePuedeAgregarUnaZonaDeCoberturaQueYaEsteEnLaPagina() {
-		ZonaDeCobertura zona = mock(ZonaDeCobertura.class);
-		
-		web.agregarZonaDeCobertura(zona);
-		web.agregarZonaDeCobertura(zona);
+	void agregarMuestraNoAgregaLaMismaMuestraDosVeces() {
+		Muestra muestra = mock(Muestra.class);
 
+		web.agregarMuestra(muestra);
+		web.agregarMuestra(muestra);
+		assertEquals(1, web.getMuestrasRegistradas().size());
+	}
+
+	@Test
+	void agregarMuestraDelegaElProcesamientoDeLaMuestraALasZonasDeCoberturaRegistradas() {
+		ZonaDeCobertura zona = mock(ZonaDeCobertura.class);
+		Muestra muestra = mock(Muestra.class);
+
+		web.agregarZonaDeCobertura(zona);
+		web.agregarMuestra(muestra);
+
+		assertEquals(1, web.getZonasDeCoberturaRegistradas().size());
+		assertEquals(1, web.getMuestrasRegistradas().size());
+
+		// Verificamos que la zona procese la muestra
+		verify(zona).procesarNuevaMuestra(muestra);
+	}
+
+	// ------------------------------------------------------------
+
+	@Test
+	void unaPaginaWebIncialmenteNoTieneZonasDeCoberturaRegistradas() {
+		assertTrue(web.getZonasDeCoberturaRegistradas().isEmpty());
+	}
+
+	@Test
+	void agregarZonaDeCoberturaAgregaUnaZonaDeCoberturaAlaPaginaWeb() {
+		ZonaDeCobertura zona = mock(ZonaDeCobertura.class);
+
+		web.agregarZonaDeCobertura(zona);
 		assertEquals(1, web.getZonasDeCoberturaRegistradas().size());
 	}
 
 	@Test
-	void agregarMuestraAgregaALaZonaDeCoberturaDondePertenescan() {
+	void agregarZonaDeCoberturaNoAgregaLaMismaZonaDosVeces() {
 		ZonaDeCobertura zona = mock(ZonaDeCobertura.class);
-		
-		when(zona.perteneceALaZona(muestra)).thenReturn(true);
-		
-		web.agregarZonaDeCobertura(zona);
-		web.agregarMuestra(muestra);
-		
-		verify(zona, times(1)).procesarNuevaMuestra(muestra);
-	}
 
-	@Test
-	void agregarMuestraNoSeAgregaALaZonaDeCoberturaDeLaPagina() {
-		ZonaDeCobertura zona = mock(ZonaDeCobertura.class);
-		
-		when(zona.perteneceALaZona(muestra)).thenReturn(false);
-		
 		web.agregarZonaDeCobertura(zona);
-		web.agregarMuestra(muestra);
-		
-		verify(zona, never()).procesarNuevaMuestra(muestra);
+		web.agregarZonaDeCobertura(zona);
+		assertEquals(1, web.getZonasDeCoberturaRegistradas().size());
 	}
 }
-
